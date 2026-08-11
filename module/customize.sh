20a5ba36555761c5962bf4c2c78dacb4400c8d03
Mintraspberry Cool <mintraspberrypony@gmail.com>
2026-08-12 03:35:45 +0800

    更新模块时结束 com.google.android.carrier

--- module/customize.sh
+++ module/customize.sh
@@ -1,2 +1,3 @@
+killall com.google.android.carrier
 rm /data/user_de/0/com.google.android.carrier/shared_prefs/com.google.android.carrier_preferences.xml
 rm /data/user_de/0/com.android.phone/files/carrierconfig-com.google.android.carrier-*.xml
