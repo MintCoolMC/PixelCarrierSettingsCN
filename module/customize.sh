edc3330a90d56231b13c2b03b36ab8aa5925fb56
Mintraspberry Cool <mintraspberrypony@gmail.com>
2026-08-12 14:33:16 +0800

    移除安装时脚本

--- module/customize.sh
+++
@@ -1,3 +0,0 @@
-killall com.google.android.carrier
-rm /data/user_de/0/com.google.android.carrier/shared_prefs/com.google.android.carrier_preferences.xml
-rm /data/user_de/0/com.android.phone/files/carrierconfig-com.google.android.carrier-*.xml
