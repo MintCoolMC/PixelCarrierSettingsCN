306ce888caedaee28a01d4c9ee16d8250051aa09
Mintraspberry Cool <mintraspberrypony@gmail.com>
2026-08-15 03:40:15 +0800

    更改为标准后缀名

--- mk_carrier_list.sh
+++ mk_carrier_list.sh
@@ -25,35 +25,35 @@ fi
 mkdir -p mkproto/protobuf mkproto/CarrierSettings 2>/dev/null
 
 if [ -n "$input_file" ]; then
-    protoc -I./mkproto -I./mkproto/include --decode=com.google.carrier.CarrierList carrier_list_1.proto < "$input_file" > mkproto/protobuf/carrier_list.proto
+    protoc -I./mkproto -I./mkproto/include --decode=com.google.carrier.CarrierList carrier_list.proto < "$input_file" > mkproto/protobuf/carrier_list.textpb
     if [ $? -ne 0 ]; then
         echo "Error: protoc decode failed" >&2
         exit 1
     fi
-    if [ -f mkproto/protobuf/carrier_list_cn.proto ]; then
-        cat mkproto/protobuf/carrier_list_cn.proto mkproto/protobuf/carrier_list.proto > mkproto/protobuf/carrier_list.proto.tmp
-        mv mkproto/protobuf/carrier_list.proto.tmp mkproto/protobuf/carrier_list.proto
+    if [ -f mkproto/protobuf/carrier_list_cn.textpb ]; then
+        cat mkproto/protobuf/carrier_list_cn.textpb mkproto/protobuf/carrier_list.textpb > mkproto/protobuf/carrier_list.textpb.tmp
+        mv mkproto/protobuf/carrier_list.textpb.tmp mkproto/protobuf/carrier_list.textpb
     else
-        echo "Error: mkproto/protobuf/carrier_list_cn.proto not found" >&2
+        echo "Error: mkproto/protobuf/carrier_list_cn.textpb not found" >&2
         exit 1
     fi
 
-    sed "s/version: [0-9]*/version: $version/g" mkproto/protobuf/carrier_list.proto > mkproto/protobuf/carrier_list.proto.tmp
-    mv mkproto/protobuf/carrier_list.proto.tmp mkproto/protobuf/carrier_list.proto
+    sed "s/version: [0-9]*/version: $version/g" mkproto/protobuf/carrier_list.textpb > mkproto/protobuf/carrier_list.textpb.tmp
+    mv mkproto/protobuf/carrier_list.textpb.tmp mkproto/protobuf/carrier_list.textpb
 else
-    previous_version=$(sed -n 's/^version: \([0-9]*\)$/\1/p' mkproto/protobuf/carrier_list.proto)
+    previous_version=$(sed -n 's/^version: \([0-9]*\)$/\1/p' mkproto/protobuf/carrier_list.textpb)
     [ -z "$previous_version" ] && previous_version=0
     version=$((previous_version + 1))
-    sed "s/version: [0-9]*/version: $version/g" mkproto/protobuf/carrier_list.proto > mkproto/protobuf/carrier_list.proto.tmp
-    mv mkproto/protobuf/carrier_list.proto.tmp mkproto/protobuf/carrier_list.proto
+    sed "s/version: [0-9]*/version: $version/g" mkproto/protobuf/carrier_list.textpb > mkproto/protobuf/carrier_list.textpb.tmp
+    mv mkproto/protobuf/carrier_list.textpb.tmp mkproto/protobuf/carrier_list.textpb
 fi
 
 current_timestamp=$(date +%s)
 sed "/last_updated {/,/}/ s/seconds: [0-9]*/seconds: $current_timestamp/" \
-    mkproto/protobuf/carrier_list.proto > mkproto/protobuf/carrier_list.proto.tmp
-mv mkproto/protobuf/carrier_list.proto.tmp mkproto/protobuf/carrier_list.proto
+    mkproto/protobuf/carrier_list.textpb > mkproto/protobuf/carrier_list.textpb.tmp
+mv mkproto/protobuf/carrier_list.textpb.tmp mkproto/protobuf/carrier_list.textpb
 
-protoc -I./mkproto -I./mkproto/include --encode=com.google.carrier.CarrierList carrier_list_1.proto < mkproto/protobuf/carrier_list.proto > mkproto/CarrierSettings/carrier_list.pb
+protoc -I./mkproto -I./mkproto/include --encode=com.google.carrier.CarrierList carrier_list.proto < mkproto/protobuf/carrier_list.textpb > mkproto/CarrierSettings/carrier_list.pb
 if [ $? -ne 0 ]; then
     echo "Error: protoc encode failed" >&2
     exit 1
--- mk_carrier_settings.sh
+++ mk_carrier_settings.sh
@@ -11,21 +11,21 @@ function generate_carrier_settings() {
         exit 1
     fi
 
-    if [  "$modify_last_updated" != "0" ]; then
+    if [  "$modify_last_updated" == "1" ]; then
         current_timestamp=$(date +%s)
         sed "/last_updated {/,/}/ s/seconds: [0-9]*/seconds: $current_timestamp/" \
         "$input_file" > "$input_file".tmp
         mv "$input_file".tmp "$input_file"
     fi
 
-    protoc -I./mkproto -I./mkproto/include --encode=com.google.carrier.CarrierSettings carrier_settings_1.proto < "$input_file" > "$output_file"
+    protoc -I./mkproto -I./mkproto/include --encode=com.google.carrier.CarrierSettings carrier_settings.proto < "$input_file" > "$output_file"
     if [ $? -ne 0 ]; then
         echo "Error: protoc encode failed for '$input_file'" >&2
         exit 1
     fi
 }
 
-generate_carrier_settings "mkproto/protobuf/cbn_cn.proto" "mkproto/CarrierSettings/cbn_cn.pb"
-generate_carrier_settings "mkproto/protobuf/chinamobile_cn.proto" "mkproto/CarrierSettings/chinamobile_cn.pb"
-generate_carrier_settings "mkproto/protobuf/chinatelecom_cn.proto" "mkproto/CarrierSettings/chinatelecom_cn.pb"
-generate_carrier_settings "mkproto/protobuf/chinaunicom_cn.proto" "mkproto/CarrierSettings/chinaunicom_cn.pb"
+generate_carrier_settings "mkproto/protobuf/cbn_cn.textpb" "mkproto/CarrierSettings/cbn_cn.pb"
+generate_carrier_settings "mkproto/protobuf/chinamobile_cn.textpb" "mkproto/CarrierSettings/chinamobile_cn.pb"
+generate_carrier_settings "mkproto/protobuf/chinatelecom_cn.textpb" "mkproto/CarrierSettings/chinatelecom_cn.pb"
+generate_carrier_settings "mkproto/protobuf/chinaunicom_cn.textpb" "mkproto/CarrierSettings/chinaunicom_cn.pb"
