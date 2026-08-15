c7ddab2e0943a0e9bc2064d1fd7e50bf33e2bddc
Mintraspberry Cool <mintraspberrypony@gmail.com>
2026-08-16 00:07:44 +0800

    1.11-DEV

--- module/customize.sh
+++ module/customize.sh
@@ -1,3 +1,7 @@
+
+
+
+
 killall com.google.android.carrier
 sed -i '/identified_carriers/d; /applied_settings_version/d' \
     /data/user_de/0/com.google.android.carrier/shared_prefs/com.google.android.carrier_preferences.xml
